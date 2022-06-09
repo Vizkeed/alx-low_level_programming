37d68dfe5e7f6099bd737829a454be47fa6d6909
vizkeed <victorekeh343@gmail.com git config --global user.name Your>
2022-06-08 17:14:02 -0700

    prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-include "_putchar"
+include "main.h"
 
 /**
  * main - print _putchar
@@ -6,7 +6,7 @@ include "_putchar"
  */
 int main(void)
 {
-_putchar('_');
+_putchar('=');
 _putchar('p');
 _putchar('u');
 _putchar('t');
